cc60841cfea49d63bd221c66293273648495a4ba
Kleyton Carv <135996798+kleytoncarv@users.noreply.github.com>
2023-08-24 11:14:42 -0300

    dps deleto
    dps deleto

---
+++ poo.py
@@ -0,0 +1,50 @@
+# #class bola:
+#     def __init__(self, cor, circunferencia, material):
+#         self.cor = cor
+#         self.circunferencia = circunferencia
+#         self.material = material
+
+#     def trocarCor(self):
+#         nova_cor = input('Digite a nova cor: ')
+#         self.cor = nova_cor
+
+#     def mostrarCor(self):
+#         return self.cor
+    
+
+# minha_bola = bola("vermelha", 10, "couro")
+# print("Cor atual da bola:", minha_bola.mostrarCor())
+
+# minha_bola.trocarCor()
+# print("Nova cor da bola:", minha_bola.mostrarCor())
+
+# class quadrado:
+#     def __init__(self, tamanho1):
+#         self.tamanho1 = int(tamanho1)
+      
+    
+#     def mudarTamanho(self):
+#         novo_Tamanho = input('Digite novo tamanho: ')
+#         self.tamanho1 = int(novo_Tamanho)
+ 
+#     def mostrarTamanho(self):
+#         return self.tamanho1
+    
+#     def area1(self):
+#         self.tamanho1 = self.tamanho1 * self.tamanho1
+#         return self.tamanho1
+    
+# meu_quadrado = quadrado(100)
+# print ('Tamanho do quadrado', meu_quadrado.mostrarTamanho())
+
+# meu_quadrado.mudarTamanho()
+# print ('Tamanho novo', meu_quadrado.mostrarTamanho())
+# print('Area é: ', meu_quadrado.area1())
+
+
+
+
+#    https://gist.github.com/Nathanbahia/e4001c25eddd472153a69bcf31febe8e
+
+
+    
